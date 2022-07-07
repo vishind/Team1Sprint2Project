26491a07bbb69d5636a7a8cc27375a0ca65e0798
Pranab04 <108386296+Pranab04@users.noreply.github.com>
2022-07-07 16:39:48 +0530

    d

--- BddInsuranceBroker/src/test/java/Register/RegisterRunner.java
+++ BddInsuranceBroker/src/test/java/Register/RegisterRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class) 
-@CucumberOptions(plugin = {"pretty", "html:target/Destination"},
+@CucumberOptions(plugin = {"pretty", "html:target/Destination","json:target/JsonReport","junit:target/xmlReport"},
 monochrome=true
 )
 public class RegisterRunner {
